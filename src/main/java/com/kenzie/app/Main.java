3d9f722863a5edaa12dbaeb65cca84cd30575a7b
Karen Wang <kwang022020@gmail.com>
2022-09-08 21:13:44 -0700

    finished WK22 Extra Practice - Stock Buying Application

--- src/main/java/com/kenzie/app/Main.java
+++ src/main/java/com/kenzie/app/Main.java
@@ -3,6 +3,14 @@ package com.kenzie.app;
 import java.util.Scanner;
 
 public class Main {
+    public static void displayMenu() {
+        System.out.println("Enter a selection (1-4)");
+        System.out.println("1. Check your Balance");
+        System.out.println("2. Buy");
+        System.out.println("3. Sell");
+        System.out.println("4. Exit");
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Kenzie Stock Exchange");
         Scanner in = new Scanner(System.in);
@@ -14,13 +22,54 @@ public class Main {
         double price = Double.parseDouble(in.nextLine());
 
         // Create your StockHolding using the values above
-
+        StockHolding stockHolding = new StockHolding(name, ticker, price);
         // Create your Execution Loop
+        boolean exitFlag = false;
+        while (!exitFlag) {
             // print the menu
+            displayMenu();
             // collect which option they chose from Scanner
+             try {
+            int userInputOption = Integer.parseInt(in.nextLine());
             // perform the operation: Check your balance, buy, sell, or exit
             // repeat!
-
+            if (userInputOption != 4) {
+                switch (userInputOption) {
+                    case 1:
+                        System.out.println("You own " + stockHolding.getNumShares() +
+                                " shares of " + stockHolding.getName() +
+                                " (" + stockHolding.getTicker() + ")");
+                        System.out.println("Your balance is: $" + stockHolding.getBalance());
+                        break;
+                    case 2:
+                        System.out.printf("How many shares would you like to buy?" + "\n");
+                        int inputNumSharesToBuy = Integer.parseInt(in.nextLine());
+                        stockHolding.buy(inputNumSharesToBuy);
+                        System.out.println("You bought " + inputNumSharesToBuy +
+                                " shares for $" + price * inputNumSharesToBuy);
+                        break;
+                    case 3:
+                        System.out.println("How many shares would you like to sell?");
+                        int inputNumSharesToSell = Integer.parseInt(in.nextLine());
+                        if (inputNumSharesToSell <= stockHolding.getNumShares()) {
+                            double amtSold = stockHolding.sell(inputNumSharesToSell);
+                            System.out.println("You sold " + inputNumSharesToSell +
+                                    " shares for $" + amtSold);
+                        } else {
+                            System.out.println("You do not have enough shares!");
+                        }
+                        break;
+                    default:
+                        System.out.println("The selection is unrecognized");
+                }
+            } else {
+                exitFlag = true;
+            }
+             }
+                 catch(NumberFormatException e) {
+                     System.out.println("The selection is unrecognized");
+                 }
+        }
         System.out.println("Goodbye!");
     }
 }
--- src/main/java/com/kenzie/app/StockHolding.java
+++ src/main/java/com/kenzie/app/StockHolding.java
@@ -2,11 +2,51 @@ package com.kenzie.app;
 
 public class StockHolding {
     // Create private instance properties
-
+    private String name;
+    private String ticker;
+    private double price;
+    private int numShares;
     // Create constructor
+
+    public StockHolding(String name, String ticker, double price) {
+        this.name = name;
+        this.ticker = ticker;
+        this.price = price;
+        this.numShares = 0;
+    }
 
     // create getters
 
-    // create buy, sell, and getBalance methods
+    public String getName() {
+        return name;
+    }
 
+    public String getTicker() {
+        return ticker;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getNumShares() {
+        return numShares;
+    }
+
+
+    // create buy, sell, and getBalance methods
+    public void buy(int shares){
+        numShares += shares;
+    }
+    public double sell(int shares){
+        double amtSold = 0.0;
+        if (shares <= numShares){
+            numShares -= shares;
+            amtSold = price*shares;
+        }
+        return amtSold;
+    }
+    public double getBalance(){
+        return price * numShares;
+    }
 }
